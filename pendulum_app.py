7f44fe726302561940db66048e222d3a63845638
pineapple-bois <91347851+pineapple-bois@users.noreply.github.com>
2024-08-05 14:29:41 +0100

    Staged for deployment

--- pendulum_app.py
+++ pendulum_app.py
@@ -39,11 +39,11 @@ app = dash.Dash(
 
 
 # Comment out to launch locally (development)
-# @server.before_request
-# def before_request():
-#     if not request.is_secure:
-#         url = request.url.replace('http://', 'https://', 1)
-#         return redirect(url, code=301)
+@server.before_request
+def before_request():
+    if not request.is_secure:
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=301)
 
 
 # App set up
