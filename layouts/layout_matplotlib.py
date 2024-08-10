845da957d8fd979e26413dde85fd123612c82676
pineapple-bois <91347851+pineapple-bois@users.noreply.github.com>
2024-08-10 12:52:51 +0100

    Changed font

--- layouts/layout_matplotlib.py
+++ layouts/layout_matplotlib.py
@@ -2,17 +2,17 @@ import plotly.graph_objs as go
 
 # Define Matplotlib layout for plotly figures
 mpl_layout = go.Layout(
-    title_font=dict(family='Courier New, monospace', size=16, color='black'),
+    title_font=dict(family='Red Hat Display, sans-serif', size=16, color='black'),
     paper_bgcolor='white',
     plot_bgcolor='white',
     xaxis=dict(
-        titlefont=dict(family='Courier New, monospace', size=14, color='black'),
+        titlefont=dict(family='Red Hat Display, sans-serif', size=14, color='black'),
         showgrid=True,
         gridcolor='lightgrey',
         fixedrange=True  # disables horizontal zoom/pan
     ),
     yaxis=dict(
-        titlefont=dict(family='Courier New, monospace', size=14, color='black'),
+        titlefont=dict(family='Red Hat Display, sans-serif', size=14, color='black'),
         showgrid=True,
         gridcolor='lightgrey',
         fixedrange=True  # disables vertical zoom/pan
