fd91274dbea05512f24d0f0ebcea4d68c1b825cd
pineapple-bois <91347851+pineapple-bois@users.noreply.github.com>
2024-08-10 18:56:55 +0100

    Refactored output

--- AppFunctions.py
+++ AppFunctions.py
@@ -2,18 +2,14 @@ from dash import html
 import plotly.tools as tls
 import matplotlib.pyplot as plt
 
-# Define a maximum time allowed for the simulation (2 minutes)
+# Constants
 MAX_TIME = 120
-
-# Define max parameter values
 MAX_LENGTH = 10      # meters
 MIN_LENGTH = 0.1     # meters
 MAX_MASS = 1000      # kilograms
 MIN_MASS = 0.1       # kilograms
-MAX_GRAVITY = 23.15  # m/s^2, g on Jupiter (2.36 * g_earth)
-MIN_GRAVITY = 0.696  # m/s^2, g on Pluto (0.071 * g_earth)
-
-# Max initial conditions (theta_i is hardcoded to be \in [-360, 360]
+MAX_GRAVITY = 23.15  # m/s^2, g on Jupiter
+MIN_GRAVITY = 0.696  # m/s^2, g on Pluto
 MAX_ANGULAR_VELOCITY = 1000
 
 
@@ -22,30 +18,8 @@ def validate_inputs(initial_conditions_list, time_start, time_end, model_type,
                     param_l1, param_l2, param_m1, param_m2, param_M1, param_M2, param_g):
     error_list = []
 
-    # Check if time values are None
-    if time_start is None:
-        error_list.append("Please provide a value for start time.")
-        error_list.append(html.Br())
-    if time_end is None:
-        error_list.append("Please provide a value for end time.")
-        error_list.append(html.Br())
-
-    # Only perform further checks if both times are not None
-    if time_start is not None and time_end is not None:
-        if time_start >= time_end or time_end <= 0:
-            error_list.append("End time must be greater than start time.")
-            error_list.append(html.Br())
-
-        if time_start < 0:
-            error_list.append("Time interval must begin at zero.")
-            error_list.append(html.Br())
-
-        if time_end - time_start > MAX_TIME:
-            error_list.append(f"Maximum simulation time is {MAX_TIME} seconds.")
-            error_list.append(html.Br())
-
-    # Validate parameters
-    # Parameter limits
+    # Parameter validation
+    param_errors = []
     param_limits = {
         'l1 (length of rod 1)': (MIN_LENGTH, MAX_LENGTH),
         'l2 (length of rod 2)': (MIN_LENGTH, MAX_LENGTH),
@@ -56,7 +30,6 @@ def validate_inputs(initial_conditions_list, time_start, time_end, model_type,
         'g (acceleration due to gravity)': (MIN_GRAVITY, MAX_GRAVITY),
     }
 
-    # Prepare parameter validation based on model type
     param_values = {
         'l1 (length of rod 1)': param_l1,
         'l2 (length of rod 2)': param_l2,
@@ -67,49 +40,71 @@ def validate_inputs(initial_conditions_list, time_start, time_end, model_type,
             param_m2 if model_type == 'simple' else param_M2,
     }
 
-    # Validate parameters
-    # Validate each parameter against its limits
     for param_name, param_value in param_values.items():
-        if param_name in param_limits:  # Ensure the parameter name exists in the limits dictionary
+        if param_name in param_limits:
             min_val, max_val = param_limits[param_name]
             if param_value is None:
-                error_list.append(f"{param_name} is required.")
-                error_list.append(html.Br())
+                param_errors.append(f"{param_name} requires a numerical value.")
             elif not isinstance(param_value, (int, float)):
-                error_list.append(f"{param_name} must be a number.")
-                error_list.append(html.Br())
+                param_errors.append(f"{param_name} must be a number.")
             elif not (min_val <= param_value <= max_val):
-                error_list.append(f"{param_name} must be between {min_val} and {max_val}.")
-                error_list.append(html.Br())
+                param_errors.append(f"{param_name} must be between {min_val} and {max_val}.")
                 if param_name == 'g (acceleration due to gravity)':
-                    error_list.append(
+                    param_errors.append(
                         f"Note: Pluto's gravity = {MIN_GRAVITY} m/s^2, Jupiter's gravity = {MAX_GRAVITY} m/s^2")
-                    error_list.append(html.Br())
 
-    # Validate initial conditions
-    # Define a mapping from index to label
-    index_to_label = {1: 'Pendulum A', 2: 'Pendulum B', 3: 'Pendulum C'}
+    if param_errors:
+        error_list.append("Parameter values:")
+        error_list.append(html.Br())
+        error_list.extend([html.Div(error) for error in param_errors])
+        error_list.append(html.Br())
+        error_list.append(html.Br())
+
+    # Initial condition validation for each pendulum
+    init_cond_errors = []
+    condition_names = ['θ1', 'θ2', 'ω1', 'ω2']
+    multiple_initial_conditions = len(initial_conditions_list) > 1
 
     for i, conditions in enumerate(initial_conditions_list, start=1):
-        condition_names = ['θ1', 'θ2', 'ω1', 'ω2']
-        pendulum_label = index_to_label.get(i, f"{i}")
+        pendulum_label = f"Pendulum {i}: " if multiple_initial_conditions else ""
         for name, value in zip(condition_names, conditions):
             if value is None or not isinstance(value, (int, float)) or isinstance(value, bool):
-                error_list.append(f"{pendulum_label}: {name} requires a numerical value.")
-                error_list.append(html.Br())
+                init_cond_errors.append(f"{pendulum_label}{name} requires a numerical value.")
             else:
                 if name in ['θ1', 'θ2'] and (value < -180 or value > 180):
-                    error_list.append(f"{pendulum_label}: {name} must be between -180 and 180 degrees.")
-                    error_list.append(html.Br())
+                    init_cond_errors.append(f"{pendulum_label}{name} must be between -180 and 180 degrees.")
                 elif name in ['ω1', 'ω2']:
                     if abs(value) > MAX_ANGULAR_VELOCITY:
-                        if value > 0:
-                            error_list.append(
-                                f"{pendulum_label}: {name} must be less than {MAX_ANGULAR_VELOCITY} deg/s.")
-                        else:
-                            error_list.append(
-                                f"{pendulum_label}: {name} must be greater than -{MAX_ANGULAR_VELOCITY} deg/s.")
-                        error_list.append(html.Br())
+                        init_cond_errors.append(
+                            f"{pendulum_label}{name} must be within ±{MAX_ANGULAR_VELOCITY} deg/s.")
+
+    if init_cond_errors:
+        error_list.append("Initial conditions:")
+        error_list.append(html.Br())
+        error_list.extend([html.Div(error) for error in init_cond_errors])
+        error_list.append(html.Br())
+
+    # Time validation
+    time_errors = []
+    if time_start is None:
+        time_errors.append("Please provide a value for start time.")
+    if time_end is None:
+        time_errors.append("Please provide a value for end time.")
+
+    if time_start is not None and time_end is not None:
+        if time_start >= time_end or time_end <= 0:
+            time_errors.append("End time must be greater than start time.")
+        if time_start < 0:
+            time_errors.append("Time interval must begin at zero.")
+        if time_end - time_start > MAX_TIME:
+            time_errors.append(f"Maximum simulation time is {MAX_TIME} seconds.")
+
+    if time_errors:
+        error_list.append("Time values:")
+        error_list.append(html.Br())
+        error_list.extend([html.Div(error) for error in time_errors])
+        error_list.append(html.Br())
+        error_list.append(html.Br())
 
     # Return the error message or None if there are no errors
     return html.Div(error_list) if error_list else None
