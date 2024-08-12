91862d7fd108e27400acbdf85b770b26ed295769
pineapple-bois <91347851+pineapple-bois@users.noreply.github.com>
2024-08-12 21:43:43 +0100

    Moved files

--- Old_App_Files/clientside.js
+++
@@ -1,9 +0,0 @@
-window.dash_clientside = Object.assign({}, window.dash_clientside, {
-     clientside: {
-         reset: function(n_clicks) {
-             if (n_clicks > 0) {
-                 window.location.reload();
-             }
-         }
-     }
-});
